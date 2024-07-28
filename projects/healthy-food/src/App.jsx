f8cc9412e886131112c56a1891ea757d6691d8ee
Manisha <manisha857522@gmail.com>
2024-07-28 10:09:57 +0530

    ok

--- projects/healthy-food/src/App.jsx
+++ projects/healthy-food/src/App.jsx
@@ -1,14 +1,18 @@
-import "./App.css";
+
 import FoodItem from "./component/fooditem";
 import Error from "./component/errormessage";
+import Container from "./component/container";
 
 function App() {
   let fooditems=["Dal","sabji","golgappa","carrot","allu"];
   return (
   <>
+  <Container>
     <h1 className="heading">healthy food</h1>
     <Error items={fooditems}></Error>
     <FoodItem items={fooditems}></FoodItem>
+    </Container>
+    <Container>hello</Container>
       
     </>
   );
