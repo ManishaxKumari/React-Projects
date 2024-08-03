699b3e275d24b230bf8b176eaae0c33ea4e0aa9e
Manisha <manisha857522@gmail.com>
2024-08-03 14:00:21 +0530

    calculator-done

--- projects/calculator/calculator/src/App.jsx
+++ projects/calculator/calculator/src/App.jsx
@@ -1,12 +1,29 @@
 import styles from "./App.module.css";
 import Display from "./components/Display";
 import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
 function App() {
+
+let [calval,setcalval]=useState("");
+const onButtonClick=(buttonText)=>{
+  if(buttonText === 'C') {
+    setcalval("");
+  }
+  else if(buttonText==='='){
+    const result = eval(calval);
+    setcalval(result);
+  }
+  else{
+    const newDisplayValue= calval + buttonText;
+    setcalval(newDisplayValue);
+  }
+}
+
   return (
     <center>
     <div className={styles.calculator}>
-      <Display></Display>
-      <ButtonsContainer></ButtonsContainer>
+      <Display displayValue={calval}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
       
 
     </div>
